a1c064f16653ddf30e6d313b17aafe813434f0cb
vidhishah98 <vidhishah101998@gmail.com>
2016-10-01 00:08:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,8 +25,11 @@ content:
     
     
 };
-
-var htmltemplate={
+function createtemplate(data)
+{
+    var title=data.title;
+    var content=data.content;
+var htmltemplate=
     <html>
     <head>
         <title>Article one</title>
@@ -55,13 +58,14 @@ var htmltemplate={
 </html>
 
 }
-
+retrn htmltemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
+    
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
