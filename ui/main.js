ccfde443ccc05f2020b6447731d0e17c51acbe00
vidhishah98 <vidhishah101998@gmail.com>
2016-11-18 18:12:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,29 @@ function moveRight(){
 img.onClick=function() {
     var interval=setInterval(moveRight,50);
 };
+
+
+
+//Counter code
+var button=document.getElementById('counter');
+button.onClick = function() {
+    //create a request object
+    var request= new XMLHttpRequest();
+    
+    
+    //Capture the response and store it in a variable
+    request.ononreaystatechange=function() {
+      if(request.readystate===XMLHttpRequest.DONE)  {
+          //Take some action
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+          }
+      }
+      //Not done yet
+    };
+    //Make the request
+    request.open('GET','http://vidhishah98.imad.hasura-app.io/counter',true);
+   request.send(null);
+};
