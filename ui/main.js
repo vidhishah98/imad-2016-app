29ffcc624e676d22c14aa177f2d8fd6ec408d2db
vidhishah98 <vidhishah101998@gmail.com>
2016-11-19 00:35:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,12 +64,12 @@ submit.onClick=function() {
                var list='';
            for(var i=0;i<=names.length;i++){
                list+="<li>"+names[i]+"</li>";
-   }
+   };
    var ul=document.getElementById('namelist');
 ul.innerHTML=list;
              
-        }
-      }
+        };
+      };
       //Not done yet
     };
     
