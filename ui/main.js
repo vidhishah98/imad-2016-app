2c72d545924918bf2c42e75eb75dfdff2b7f17ac
vidhishah98 <vidhishah101998@gmail.com>
2016-11-18 17:40:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML="NEW VALUE";
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onClick=function() {
