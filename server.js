d395f865da91a1058b5fb0c4e5b2b0f790adc83d
vidhishah98 <vidhishah101998@gmail.com>
2016-11-18 17:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ retrn htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+counter=counter+1;
+  res.send(counter.toString()); 
+});
 app.get('/article-one', function (req, res) {
 res.send(createTemplate(articleOne));
     
